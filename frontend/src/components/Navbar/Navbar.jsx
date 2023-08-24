1bfc1b292f926a9eba2f06983928119a026935f7
Sophia Zhang <zhan0531@algonquinlive.com>
2023-08-24 14:26:10 -0400

    revert "made changes to adaptive CSS, incorporated code for scrolling to sections with navbar buttons."
    This reverts commit b565aec611c70a37c8f98fd2abe744e8b945bb99.

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import Login from './Login/Login';
 const DatamlNavbar = () => {
   return (
     <>
-      <Navbar sticky="top" expand="lg" bg="dark" variant="dark">
+      <Navbar expand="lg" bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Data ML Project</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" /> {/* sandwich toggle for when screen resolution is equivalent to mobile devices */}
--- frontend/src/views/LandingPage/LandingPage.jsx
+++ frontend/src/views/LandingPage/LandingPage.jsx
@@ -7,7 +7,6 @@ import './LandingPage.scss';
 
 
 
-
 const LandingPage = () => {
   return (
     <div className='landing-page'>

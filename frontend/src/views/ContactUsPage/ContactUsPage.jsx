848af00ec0cf1d505c297a7ef1de72cf749b4dce
Manveer Singh <sing1474@algonquinlive.com>
2023-08-25 10:54:40 -0400

    I have modified contact us form

--- frontend/src/views/ContactUsPage/ContactUsPage.jsx
+++ frontend/src/views/ContactUsPage/ContactUsPage.jsx
@@ -1,20 +1,19 @@
 import React, { useState } from 'react';
-import dynamodb from './awsConfig'; // Import the AWS SDK configuration
+import dynamodb from './awsConfig';
 import './ContactUsPage.scss';
 import { v4 as uuidv4 } from 'uuid';
 
 const ContactUsPage = () => {
-
-  const uuid = uuidv4(); 
+  const uuid = uuidv4();
 
   const [formData, setFormData] = useState({
     uuid: uuid,
     name: '',
-    email: '', 
+    email: '',
     question: '',
   });
 
-
+  const [submitted, setSubmitted] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -22,57 +21,60 @@ const ContactUsPage = () => {
   };
 
   const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // Save the form data to DynamoDB
+      dynamodb.put({
+        TableName: 'ContactForm',
+        Item: {
+          uuid: uuid,
+          name: formData.name,
+          email: formData.email,
+          question: formData.question,
+        },
+      }).promise();
 
-      e.preventDefault();
-      try {
-        // Save the form data to DynamoDB
-        dynamodb.put({
-          TableName: 'ContactForm', // Replace with your actual DynamoDB table name
-          Item: {
-            uuid: uuid,
-            name: formData.name,
-            email: formData.email,
-            question: formData.question,
-          },
-        }).promise();
-        console.log('Data submitted successfully');
-      } catch (error) {
-        console.error('Error submitting data', error);
-      }
+      // Clear form data and set submitted state to true
+      setFormData({
+        uuid: uuidv4(),
+        name: '',
+        email: '',
+        question: '',
+      });
+      setSubmitted(true);
 
+      console.log('Data submitted successfully');
+    } catch (error) {
+      console.error('Error submitting data', error);
+    }
   };
-
 
   return (
     <div className="contactus">
       <h1>Contact Us</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
-          </div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+        </div>
 
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
-          </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+        </div>
 
-          <div className="form-group">
-            <label htmlFor="question">Question:</label>
-            <textarea id="question" name="question" rows="4" value={formData.question} onChange={handleChange} required />
-          </div>
+        <div className="form-group">
+          <label htmlFor="question">Question:</label>
+          <textarea id="question" name="question" rows="4" value={formData.question} onChange={handleChange} required />
+        </div>
 
-          <div className="form-buttons">
-            <button type="submit">Submit</button>
-            {/* <button type="button" onChange="">Cancel</button> */}
-          </div>
-        </form>
-       
-      </div>
-  )
-}
+        <div className="form-buttons">
+          <button type="submit">Submit</button>
+        </div>
+        {submitted && <p className="thank-you-message">Thanks for contacting us</p>}
+      </form>
+    </div>
+  );
+};
 
-export default ContactUsPage
-
-
-//  value={formData.name} value={formData.email} value={formData.question}
+export default ContactUsPage;

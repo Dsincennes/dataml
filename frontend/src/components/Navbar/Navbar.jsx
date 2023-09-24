21334ee536c180eec8ddea6d4a3c2e2cb2d76f07
dsincennes <Donaldsincennes@hotmail.com>
2023-09-24 02:09:10 -0400

    Fixed routing

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 import './Navbar.scss'
 
 const DatamlNavbar = () => {
@@ -14,14 +15,14 @@ const DatamlNavbar = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" /> {/* sandwich toggle for when screen resolution is equivalent to mobile devices */}
           <Navbar.Collapse id="basic-navbar"> {/* collapsed navbar */}
             <Nav className="me-auto">
-              <Nav.Link href="#image">Home</Nav.Link>
-              <Nav.Link href="#services">Services</Nav.Link>
+              <HashLink to="/#image" className="nav-link">Home</HashLink>
+              <HashLink to="/#services" className="nav-link">Services</HashLink>
               <Link to="/views/directorship" className="nav-link">
                 Directorship
-              </Link>
-              <Nav.Link href="#projects">Products</Nav.Link>
-              <Nav.Link href="#aboutus">About Us</Nav.Link>
-              <Nav.Link href="#contactus">Contact Us</Nav.Link>
+              </Link>z
+              <HashLink to="/#projects"  offset={-160} className="nav-link">Products</HashLink>
+              <HashLink to="/#aboutus" className="nav-link">About Us</HashLink>
+              <HashLink to="/#contactus" className="nav-link">Contact Us</HashLink>
               <a href='https://dataml.auth.us-east-1.amazoncognito.com/login?client_id=2bcudra8uccse9hh1dceu5hci8&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fdataml.io%2F'
                 className="nav-link login-button">Login</a>
             </Nav>

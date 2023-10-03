2be48e7825b4d308f2c9600f61ab35bf3418ee49
Sophia Zhang <zhan0531@algonquinlive.com>
2023-10-03 13:22:13 -0400

    added navbar for analysis pages to all services

--- frontend/src/views/Services/GeoDemographic.jsx
+++ frontend/src/views/Services/GeoDemographic.jsx
@@ -1,27 +1,133 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './Services.scss';
+//import GoogleCollab from ''
 
 const GeoDemographic = () => {
     return (
         <div className="geodemographic">
             <Navbar />
-            <head>
-                <title>Geodemographic Analysis</title>
-            </head>
-            <body>
-                <header>
-                    <h1>Welcome to the Geodemographic Analysis Page!</h1>
-                </header>
 
-                <main>
-                    <h2>Content Section</h2>
-                    <p>This is a basic HTML page. You can replace this content with your own.</p>
-                </main>
+            <div className='main-content'>
+                <head>
+                    <title>Geodemographic Analysis</title>
+                </head>
+                <Nav className="analysisNav" >
+                    {/* <h3>GeoDemographic Analysis</h3> */}
+                    <Nav.Link>Demo 1</Nav.Link>
+                </Nav>
 
-                <footer>
-                    <p>&copy; 2023 Data Machine Learning. All rights reserved.</p>
-                </footer>
-            </body>
+                <body>
+                    <header>
+                        <h1>Welcome to the Geodemographic Analysis Page!</h1>
+                    </header>
+
+                    <main>
+
+                        <h2>Content Section</h2>
+                        <p>This is a basic HTML page. You can replace this content with your own.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                                Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                                Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                                Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                                Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                                Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                                Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                                Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                                Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                                Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+                            Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+                            Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+                            Suspendisse sit amet ex vel elit cursus lobortis.</p>
+                    </main>
+
+                    <footer>
+                        <p>&copy; 2023 Data Machine Learning. All rights reserved.</p>
+                    </footer>
+                </body>
+            </div>
+
         </div>
     )
 }
--- frontend/src/views/Services/Marketing.jsx
+++ frontend/src/views/Services/Marketing.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import Nav from 'react-bootstrap/Nav'; 
+import './Services.scss'; // Services page CSS 
 
 const Marketing = () => {
   return (
     <div className="trend">
       <Navbar />
-      <h1>Marketing analysis</h1>
-      <p>
-        Marketing analysis will be displayed here with google colab
-      </p>
+
+      <div className='main-content'>
+        <head>
+          <title>Geodemographic Analysis</title>
+        </head>
+        <Nav className="analysisNav" >
+          {/* <h3>GeoDemographic Analysis</h3> */}
+          <Nav.Link>Test 1</Nav.Link>
+        </Nav>
+        <body>
+          <header>
+            <h1>Welcome to the Geodemographic Analysis Page!</h1>
+          </header>
+
+          <main>
+
+            <h2>Content Section</h2>
+            <p>This is a basic HTML page. You can replace this content with your own.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas leo in lorem luctus, non laoreet sapien porttitor.
+              Integer a ex elit. Proin lacinia nunc eleifend felis tempor volutpat. Fusce augue felis, porta eget leo vitae, malesuada efficitur erat.
+              Quisque accumsan ante quis eros sodales maximus. Suspendisse sed maximus ligula. Donec risus felis, consectetur in accumsan tempus, fermentum eu arcu.
+              Suspendisse sit amet ex vel elit cursus lobortis.</p>
+          </main>
+
+          <footer>
+            <p>&copy; 2023 Data Machine Learning. All rights reserved.</p>
+          </footer>
+        </body>
+      </div>
     </div>
   )
 }

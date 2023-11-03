2a37cec76b49b148f93bd7c2016b254b28553004
Dylan-Prust <123534901+Dylan-Prust@users.noreply.github.com>
2023-11-03 11:16:42 -0400

    Embedded Mercury into analysis page

--- frontend/src/views/Services/Trend.jsx
+++ frontend/src/views/Services/Trend.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 
 const TrendPage = () => {
+  // Replace 'your-ec2-instance-public-ip' with the public IP of your EC2 instance
+  // and 'port' with the port number you've configured Jupyter to run on (default is 8888).
+  // Make sure your file path is correct: it should point to a .ipynb file, not a .py file.
+  const notebookUrl = `http://18.207.143.203:8888/notebooks/Data_Test.ipynb`;
+
   return (
     <div className="trend">
       <Navbar />
       <h1>Trend analysis</h1>
       <p>
-        Trend analysis will be displayed here with google colab
+        Trend analysis will be displayed here
       </p>
+      <iframe src="http://dp.runmercury.com" height="700px" width="1200px"></iframe>
     </div>
   )
 }
 
-export default TrendPage
+export default TrendPage;

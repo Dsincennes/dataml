d150bd704b488c10cf58d67e0530214329993c97
Sophia Zhang <zhan0531@algonquinlive.com>
2023-11-16 15:37:24 -0500

    updated public IP in geodemo.

--- frontend/src/views/Services/GeoDemographic.jsx
+++ frontend/src/views/Services/GeoDemographic.jsx
@@ -9,7 +9,7 @@ const GeoDemographic = () => {
 
     const redirectToURL = () => {
         // Redirecting to streamlit in EC2 instance
-        window.location.href = 'http://18.217.43.98:8501';
+        window.location.href = 'http://18.118.169.8:8501';
       };
       
     const geodemoMenu = {

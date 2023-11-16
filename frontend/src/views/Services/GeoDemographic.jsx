6899047163076cec1d5ad8e69bfe106931a390db
Sophia Zhang <zhan0531@algonquinlive.com>
2023-11-16 11:09:03 -0500

    implemented dashboard in analysis page and redirection to streamlit app

--- frontend/src/views/Services/GeoDemographic.jsx
+++ frontend/src/views/Services/GeoDemographic.jsx
@@ -1,33 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import './GeoDemographic.scss';
+import './SocialMedia.css';
+
 
 const GeoDemographic = () => {
+    const [selectedOption, setSelectedOption] = useState('Analysis');
+
+    const redirectToURL = () => {
+        // Redirecting to streamlit in EC2 instance
+        window.location.href = 'http://3.131.85.73:8501/';
+      };
+      
+    const geodemoMenu = {
+        Analysis: (
+            <div> 
+                <div>
+                    <h3>Click below to run the housing analysis</h3>
+                    <button onClick={redirectToURL}>Open Streamlit</button>
+                </div>
+            </div>
+        ), 
+        Documentation: (
+            <div> 
+                <div>
+                    <h3>Storing good to know documentation here for general users or things I learned along the way</h3>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div className="geodemographic">
             <Navbar />
             <head>
                 <title>Geodemographic Analysis</title>
+                
             </head>
             <body class='geodemo-container' >
                 <header>
                     <br></br>
                     <h1>Welcome to the Geodemographic Analysis Page</h1>
                     <br></br>
+                    <p>Select an option below:</p>
                 </header> 
+                <div className="geodemo-dashboard">
+                        <div className="sidebar">
+                            <ul>
+                                {Object.keys(geodemoMenu).map((option) => (
+                                    <li
+                                        key={option}
+                                        className={selectedOption === option ? 'active' : ''}
+                                        onClick={() => {
+                                            setSelectedOption(option);
+                                        }}
+                                    >
+                                        {option}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
 
-                <main class='geodemo-container'>
-                    <iframe 
-                        src="https://geodemographic-analysis.runmercury.com/" 
-                        title="Geodemographic Analysis Examples" 
-                        class='iframe-style'> </iframe>
-
-                </main>
-
+                        <div className="content">
+                            {geodemoMenu[selectedOption]}
+                        </div>
+                    
+                </div>
                 <footer>
                     <p>&copy; 2023 Data Machine Learning. All rights reserved.</p>
-                </footer>
+                </footer>                
             </body>
+            
         </div>
     )
 }

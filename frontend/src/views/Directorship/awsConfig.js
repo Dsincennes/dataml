ef7607da750ac7f47482caa69875a8b6c28e6829
dsincennes <Donaldsincennes@hotmail.com>
2023-12-04 20:15:16 -0500

    update config

--- frontend/src/views/Directorship/awsConfig.js
+++ frontend/src/views/Directorship/awsConfig.js
@@ -3,8 +3,8 @@ const AWS = require('aws-sdk');
 
 AWS.config.update({
   region: 'us-east-1',
-  accessKeyId: 'AKIASCNR72RXAHEOBUTY',
-  secretAccessKey: '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',
+  accessKeyId: 'AKIASCNR72RXB6TANNMC',
+  secretAccessKey: '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',
 });
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
